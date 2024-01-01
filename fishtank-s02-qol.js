19054c8eb0ec7da0a0e945e3359e13f001d9930b
Barrett Otte <barrettotte@gmail.com>
2024-01-01 13:16:34 -0500

    bumps version number

--- fishtank-s02-qol.js
+++ fishtank-s02-qol.js
@@ -2,7 +2,7 @@
 // @name         fishtank-s02 QoL
 // @description  Adds some quality of life features for Fishtank season 2 site
 // @namespace    http://tampermonkey.net/
-// @version      2023-12-25
+// @version      2024-01-01
 // @author       barrettotte
 // @match        *://*.fishtank.live/*
 // @run-at       document-idle
